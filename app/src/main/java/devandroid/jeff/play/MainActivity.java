c8634c842d97762c77c5f56eee1b16d10c790756
Jeferson Junio <jjsconstantino@gmail.com>
2023-05-19 11:00:44 -0300

    Primeiro commit

---
+++ app/src/main/java/devandroid/jeff/play/MainActivity.java
@@ -0,0 +1,16 @@
+package devandroid.jeff.play;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        System.out.println("Teste");
+    }
+}
